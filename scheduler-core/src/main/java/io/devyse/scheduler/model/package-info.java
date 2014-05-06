4c2f7db0c609544f1bdb7ae1a3969d417d398597
Mike Reinhold <mike@coursescheduler.io>
2014-05-05 20:51:45 -0400

    Initial add of the Model package

---
+++ scheduler-core/src/main/java/io/devyse/scheduler/model/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Data model interfaces and abstract base classes for working with
+ * university and course data.
+ * 
+ * @author Mike Reinhold
+ * 
+ * @since 0.0.1
+ */
+package io.devyse.scheduler.model;
